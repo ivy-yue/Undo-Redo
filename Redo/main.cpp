765fcbd6e8c35d1b3f2653570514598d68e83173
wangyue <wangyue@macbook-air.local>
2016-12-13 19:49:36 +0800

    Initial Commit

---
+++ Redo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Redo
+//
+//  Created by wangyue on 2016/12/13.
+//  Copyright © 2016年 ___ivy___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
